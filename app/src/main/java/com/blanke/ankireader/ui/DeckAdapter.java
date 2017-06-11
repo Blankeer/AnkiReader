3f741f56d2cdba1d316bdc3fac6d692b998807d5
Blanke <blankeeee@gmail.com>
2017-06-11 13:10:44 +0800

    [ADD] add deck 多选

--- app/src/main/java/com/blanke/ankireader/ui/DeckAdapter.java
+++ app/src/main/java/com/blanke/ankireader/ui/DeckAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.afollestad.dragselectrecyclerview.IDragSelectAdapter;
 import com.blanke.ankireader.R;
 import com.blanke.ankireader.bean.Deck;
 
@@ -16,18 +17,76 @@ import java.util.List;
  * Created by blanke on 2017/6/11.
  */
 
-public class DeckAdapter extends RecyclerView.Adapter<DeckAdapter.ViewHold> {
+public class DeckAdapter extends RecyclerView.Adapter<DeckAdapter.ViewHold>
+        implements IDragSelectAdapter {
+
+    interface Listener {
+
+        void onClick(int index);
+
+        void onLongClick(int index);
+
+        void onSelectionChanged(int selectedCount);
+
+    }
+
     private Context context;
+
     private List<Deck> datas;
+    private List<Integer> selectPositios;
+    private Listener listener;
 
     public DeckAdapter(Context context) {
         this.context = context;
         datas = new ArrayList<>();
+        selectPositios = new ArrayList<>();
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    public boolean isSelected() {
+        return selectPositios.size() > 0;
+    }
+
+    public void clearSelected() {
+        selectPositios.clear();
+        notifyDataSetChanged();
+    }
+
+    public void toggleSelected(int position) {
+        if (selectPositios.contains(position)) {
+            selectPositios.remove((Integer) position);
+        } else {
+            selectPositios.add(position);
+        }
+        notifyItemChanged(position);
+        notifySelectedCount();
+    }
+
+    public int getSelectedCount() {
+        return selectPositios.size();
+    }
+
+    public void selectAll() {
+        selectPositios.clear();
+        for (int i = 0; i < getItemCount(); i++) {
+            selectPositios.add(i);
+        }
+        notifySelectedCount();
+        notifyDataSetChanged();
     }
 
     public void setDatas(List<Deck> datas) {
         this.datas = datas;
         notifyDataSetChanged();
+    }
+
+    private void notifySelectedCount() {
+        if (listener != null) {
+            listener.onSelectionChanged(selectPositios.size());
+        }
     }
 
     @Override
@@ -37,8 +96,43 @@ public class DeckAdapter extends RecyclerView.Adapter<DeckAdapter.ViewHold> {
     }
 
     @Override
-    public void onBindViewHolder(ViewHold holder, int position) {
+    public void onBindViewHolder(ViewHold holder, final int position) {
         holder.tv.setText(datas.get(position).getName());
+        holder.itemView.setSelected(selectPositios.contains(position));
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (listener != null) {
+                    listener.onLongClick(position);
+                    return true;
+                }
+                return false;
+            }
+        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onClick(position);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void setSelected(int index, boolean selected) {
+        if (selected) {
+            selectPositios.add(index);
+        } else {
+            selectPositios.remove((Integer) index);
+        }
+        notifyItemChanged(index);
+        notifySelectedCount();
+    }
+
+    @Override
+    public boolean isIndexSelectable(int index) {
+        return true;
     }
 
     @Override
--- app/src/main/java/com/blanke/ankireader/ui/MainActivity.java
+++ app/src/main/java/com/blanke/ankireader/ui/MainActivity.java
@@ -2,18 +2,21 @@ package com.blanke.ankireader.ui;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewStub;
 import android.widget.Toast;
 
+import com.afollestad.dragselectrecyclerview.DragSelectRecyclerView;
+import com.afollestad.materialcab.MaterialCab;
 import com.blanke.ankireader.R;
 import com.blanke.ankireader.bean.Deck;
 import com.blanke.ankireader.data.AnkiManager;
@@ -27,13 +30,18 @@ import com.orhanobut.logger.Logger;
 import java.io.File;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener,
+        DeckAdapter.Listener, MaterialCab.Callback {
     private AppCompatButton buStart;
     private AppCompatButton buStop;
 
     private Toolbar toolbar;
-    private RecyclerView mainRv;
-    private DeckAdapter madaAdapter;
+    private DragSelectRecyclerView mainRv;
+    private ViewStub cabStub;
+
+    private DeckAdapter mAdapter;
+    private MaterialCab cab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +56,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buStart.setOnClickListener(this);
         buStop.setOnClickListener(this);
+        cabStub = (ViewStub) findViewById(R.id.cab_stub);
+
         initRecyclerView();
 
         Acp.getInstance(this).request(new AcpOptions.Builder()
@@ -69,10 +79,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initRecyclerView() {
-        mainRv = (RecyclerView) findViewById(R.id.main_rv);
+        mainRv = (DragSelectRecyclerView) findViewById(R.id.main_rv);
         mainRv.setLayoutManager(new LinearLayoutManager(this));
-        madaAdapter = new DeckAdapter(this);
-        mainRv.setAdapter(madaAdapter);
+        mAdapter = new DeckAdapter(this);
+        mAdapter.setListener(this);
+        mainRv.setAdapter(mAdapter);
     }
 
     private void prepareLoadData() {
@@ -86,7 +97,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (decks == null || decks.size() == 0) {
             Toast.makeText(this, "牌组为空...", Toast.LENGTH_LONG).show();
         }
-        madaAdapter.setDatas(decks);
+        mAdapter.setDatas(decks);
     }
 
     private void startPlayService() {
@@ -114,6 +125,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    public void onClick(int index) {
+        if (mAdapter.isSelected()) {
+            mAdapter.toggleSelected(index);
+        }
+    }
+
+    @Override
+    public void onLongClick(int index) {
+        if (mAdapter.isSelected()) {
+            mAdapter.toggleSelected(index);
+        } else {
+            mainRv.setDragSelectActive(true, index);
+        }
+    }
+
+    @Override
+    public void onSelectionChanged(int count) {
+        if (mAdapter.isSelected()) {
+            if (cab == null) {
+                cab = new MaterialCab(this, R.id.cab_stub)
+                        .setMenu(R.menu.menu_selected_deck)
+                        .setCloseDrawableRes(R.drawable.ic_arrow_back)
+                        .start(this);
+                cab.getToolbar().setTitleTextColor(Color.WHITE);
+            }
+            cab.setTitle(mAdapter.getSelectedCount() + "");
+        } else if (cab != null && cab.isActive()) {
+            cab.reset().finish();
+            cab = null;
+        }
+    }
+
+    @Override
+    public boolean onCabCreated(MaterialCab cab, Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onCabItemClicked(MenuItem item) {
+        if (item.getItemId() == R.id.select_deck_done_all) {
+            mAdapter.selectAll();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCabFinished(MaterialCab cab) {
+        mAdapter.clearSelected();
+        this.cab = null;
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mAdapter.isSelected()) {
+            mAdapter.clearSelected();
+            cab.reset().finish();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    private void start(Class activityClass) {
+        Intent intent = new Intent(this, activityClass);
+        startActivity(intent);
+    }
+
     //test
     private void getFile() {
         String path = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separatorChar
@@ -127,7 +207,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Logger.d(f.getName());
         }
     }
-
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -145,10 +224,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         return true;
     }
-
-    private void start(Class activityClass) {
-        Intent intent = new Intent(this, activityClass);
-        startActivity(intent);
-    }
-
 }
